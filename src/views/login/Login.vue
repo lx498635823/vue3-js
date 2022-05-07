2cfe6089567dfd0af66324e10988e7f82c4abf0a
e <e@qq.com>
2022-05-07 14:05:50 +0800

    全局样式配置

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,5 +1,8 @@
 <template>
+<div>
   <div class="login">登录</div>
+  <div><a-button>123</a-button></div>
+</div>  
 </template>
 
 <script>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  //关闭语法自动检测
+  lintOnSave: false,
+  publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
+  //打包目录
+  outputDir: process.env.NODE_ENV === 'dist' ? '' : 'devDist',
+  css: {
+    loaderOptions: {
+      css: {
+        // 这里的选项会传递给 css-loader
+      },
+      postcss: {
+        // 这里的选项会传递给 postcss-loader
+      },
+      scss:{
+        additionalData: `@import "./src/style/main.scss";`
+      },
+    }
+  }
 });
