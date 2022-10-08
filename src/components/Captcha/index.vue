a5864a4f03f9e7fe9772b32733a29a292f52dcc1
unknown <498635823@qq.com>
2022-10-08 17:11:24 +0800

    改滑动验证

--- src/components/Captcha/index.vue
+++ src/components/Captcha/index.vue
@@ -12,11 +12,11 @@ export default {
     name:"Captcha",
     setup() {
         onMounted(()=>{
-            var nc_token = ["FFFF0N00000000009931", (new Date()).getTime(), Math.random()].join(':');  // 没有做任何的响应
+            var nc_token = ["FFFF0N0000000000B00D", (new Date()).getTime(), Math.random()].join(':');  // 没有做任何的响应
             var NC_Opt = 
             {
                 renderTo: "#captcha-dom",
-                appkey: "FFFF0N00000000009931",
+                appkey: "FFFF0N0000000000B00D",
                 scene: "nc_login",
                 token: nc_token,
                 customWidth: 300,
